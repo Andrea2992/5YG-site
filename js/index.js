0c5c44ed107a7a0187097ca791d20944b74a2a11
Andrea2992 <redbaron2992@gmail.com>
2023-05-24 14:17:51 +0200

    funziona

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
-(function(){
+(function() {
     
     var menu = $('#menu');
     var nav = $('#nav');
     var $window = $(window);
+    var $navBar = $('#navBar');
     
     function closeFullNav() {
         menu.removeClass('show').addClass('hide');
@@ -26,25 +27,39 @@
         nav.detach();
         nav.addClass('navFullScreen');
         menu.append(nav);
-        $('#navBar').on('click', toggleVisibility)
+        $navBar.on('click', toggleVisibility);
         nav.on('click', closeFullNav);
     }
 
     $window.on('resize', function() {
         if (window.innerWidth < 960) {
             if (nav.hasClass('navFullScreen')) {
-                
+                /***empty***/
             } else {
-                $('#nav').detach();
+                nav.detach();
                 menu.append(nav);
                 nav.addClass('navFullScreen');
-                if ($._data(document.getElementById('nav'), "events")) {
-                    
+                var navEvent = $._data(document.getElementById('nav'), "events");
+                if (navEvent) {
+                    /***empty***/
                 } else {
                     nav.on('click', closeFullNav);
+                    $navBar.on('click', toggleVisibility);
                 }
             }
         } else {
+            if (nav.hasClass('navFullScreen')) {
+                nav.detach();
+                $('#header').append(nav);
+                nav.removeClass('navFullScreen');
+                if (menu.hasClass('show')) {
+                    menu.removeClass('show').addClass('hide');
+                    $('#navList').removeClass('textAlignLeft').addClass('textAlignRight');
+                    $('#navList').children().removeClass('block').addClass('inlineBlock');
+                }
+            } else {
+                /***empty***/
+            }
             
         }
     })
