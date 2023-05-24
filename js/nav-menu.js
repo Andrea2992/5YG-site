0f0e283ce90b9c82b7d6b1947dd2ea98b9d9e48d
Andrea2992 <redbaron2992@gmail.com>
2023-05-24 21:36:29 +0200

    Merge commit '6fc8ec91f4519322fdbe90a7ea63a47e24ff6945'

---
+++ js/nav-menu.js
@@ -0,0 +1,46 @@
+(function() {
+    
+    var menu = $('#menu');
+    var $navBar = $('#navBar');
+    var navLargeScreen = $('#navLargeScreen');
+    var navSmallScreen = $('#navSmallScreen');
+    var $window = $(window);
+    
+    $navBar.on('click', toggleVisibility);
+    navSmallScreen.on('click', closeFullNav);
+    menu.on('click', closeFullNav);
+
+    function toggleVisibility() {
+        if (menu.hasClass('hide')) {
+            menu.removeClass('hide');
+        } else {
+            menu.addClass('hide');
+        }
+    }
+
+    function closeFullNav() {
+        menu.addClass('hide');
+    }
+
+    if (window.innerWidth < 960) {
+        navLargeScreen.addClass('hide');
+    }
+
+    $window.on('resize', function() {
+        if (window.innerWidth < 960) {
+            if (!navLargeScreen.hasClass('hide')) {
+                navLargeScreen.addClass('hide');
+            }
+        } else {
+            if (!menu.hasClass('hide')) {
+                menu.addClass('hide');
+            }
+            if (navLargeScreen.hasClass('hide')) {
+                navLargeScreen.removeClass('hide');
+            }
+        }
+    })
+
+
+
+}())
