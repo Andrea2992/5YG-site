ed25a8dd765bfd1c7ce9ada6f460577b8b4aac82
Andrea2992 <redbaron2992@gmail.com>
2023-06-05 18:13:38 +0200

    mod. layout sezione foto, prova sfondi senza fixed

--- js/foto.js
+++ js/foto.js
@@ -1,63 +1,73 @@
 (function() {
-var $window = $(window);
+    var $window = $(window);
 
+    function centerArrow() {    
+        var leftArrow = $('.freccia-album-left');
+        var rightArrow = $('.freccia-album-right');
+        var value;
+        var valueText;
+        var heightGalleria = $('.album-galleria').outerHeight();
+        if (window.innerWidth < 300) {
+            value = (heightGalleria / 2) - 22.5;
+        } else if (window.innerWidth < 960) {
+            value = (heightGalleria / 2) - 27.5;
+        } else {
+            value = (heightGalleria / 2) - 20;
+        }
+        valueText = value.toString() + 'px'
+        leftArrow.css({'top': valueText});
+        rightArrow.css({'top': valueText});
 
-function centerArrow() {    
-var leftArrow = $('.freccia-album-left');
-var rightArrow = $('.freccia-album-right');
-var value;
-var valueText;
-var heightGalleria = $('.album-galleria').outerHeight();
-if (window.innerWidth < 500) {
-    console.log($window.innerWidth);
-    value = (heightGalleria / 2) - 12;
-} else {
-    console.log(window.innerWidth);
-    value = (heightGalleria / 2) - 20;
-}
-valueText = value.toString() + 'px'
-leftArrow.css({'top': valueText});
-rightArrow.css({'top': valueText});
-
-$window.on('resize', function() {
-    var heightGalleria = $('.album-galleria').outerHeight();
-    if (window.innerWidth < 500) {
-        console.log($window.innerWidth);
-        value = (heightGalleria / 2) - 12;
-    } else {
-        console.log(window.innerWidth);
-        value = (heightGalleria / 2) - 20;
+        $window.on('resize', function() {
+            var heightGalleria = $('.album-galleria').outerHeight();
+            if (window.innerWidth < 300) {
+                value = (heightGalleria / 2) - 22.5;
+            } else if (window.innerWidth < 960) {
+                value = (heightGalleria / 2) - 27.5;
+            } else {
+                value = (heightGalleria / 2) - 20;
+            }
+            valueText = value.toString() + 'px'
+            leftArrow.css({'top': valueText});
+            rightArrow.css({'top': valueText});
+        });
     }
-    valueText = value.toString() + 'px'
-    leftArrow.css({'top': valueText});
-    rightArrow.css({'top': valueText});
-});
-}
+
+    function elemWidth() {
+        var elemSx = $('.elem-album-sx');
+        var elemDx = $('.elem-album-dx');
+        var elemCn = $('.elem-album-cn');
+        var widthElemSx = elemSx.width();
+        var widthElemDx = elemDx.width();
+        var widthElemCn = elemCn.width();
+        if (window.innerWidth < 960) {
+            var newWidthElemCn = widthElemCn / 1.4;
+            elemCn.css({'height': newWidthElemCn});
+        } else {
+            elemSx.css({'height': widthElemSx});
+            elemDx.css({'height': widthElemDx});
+            elemCn.css({'height': widthElemCn});
+        }
+
+        $window.on('resize', function() {
+            var widthElemSx = elemSx.width();
+            var widthElemDx = elemDx.width();
+            var widthElemCn = elemCn.width();
+            if (window.innerWidth < 960) {
+                var newWidthElemCn = widthElemCn / 1.4;
+                elemCn.css({'height': newWidthElemCn});
+            } else {
+                elemSx.css({'height': widthElemSx});
+                elemDx.css({'height': widthElemDx});
+                elemCn.css({'height': widthElemCn});
+            }
+        });
+    }
 
 
 
-var widthElemSx = $('.elem-album-sx').width();
-$('.elem-album-sx').css({'height': widthElemSx});
-var widthElemDx = $('.elem-album-dx').width();
-$('.elem-album-dx').css({'height': widthElemDx});
-var widthElemCn = $('.elem-album-cn').width();
-$('.elem-album-cn').css({'height': widthElemCn});
 
-$window.on('resize', function() {
-    var widthElemSx = $('.elem-album-sx').width();
-    $('.elem-album-sx').css({'height': widthElemSx});
-    var widthElemDx = $('.elem-album-dx').width();
-    $('.elem-album-dx').css({'height': widthElemDx});
-    widthElemCn = $('.elem-album-cn').width();
-    $('.elem-album-cn').css({'height': widthElemCn});
-    
-});
-
-
-
-
-
-
-centerArrow();
+    elemWidth();
+    centerArrow();
 
 }())
