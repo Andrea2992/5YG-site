45bd3a19666043d21c9fbf8a4019dd5ad849ba81
Andrea2992 <redbaron2992@gmail.com>
2023-06-08 09:24:07 +0200

    aggiunte immagini galleria, modifiche layout foto

---
+++ js/foto-album.js
@@ -0,0 +1,282 @@
+var foto = [
+    {
+        albumSrc: 'img/foto-cover-album/vecchiaFormazione-cover-album.jpg',
+        fotoSrc: [
+            {
+                src: 'img/vecchia-formazione/vf-1.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/vecchia-formazione/vf-2.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/vecchia-formazione/vf-3.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/vecchia-formazione/vf-4.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/vecchia-formazione/vf-5.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/vecchia-formazione/vf-6.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/vecchia-formazione/vf-7.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/vecchia-formazione/vf-8.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/vecchia-formazione/vf-9.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/vecchia-formazione/vf-10.jpg',
+                isLoading: true
+            }
+        ] 
+    },
+    {
+        albumSrc: 'img/foto-cover-album/torreano-cover-album.jpg',
+        fotoSrc: [
+            {
+                src: 'img/torreano/t-1.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/torreano/t-2.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/torreano/t-3.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/torreano/t-4.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/torreano/t-5.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/torreano/t-6.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/torreano/t-7.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/torreano/t-8.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/torreano/t-9.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/torreano/t-10.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/torreano/t-11.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/torreano/t-12.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/torreano/t-13.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/torreano/t-14.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/torreano/t-15.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/torreano/t-16.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/torreano/t-17.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/torreano/t-18.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/torreano/t-19.jpg',
+                isLoading: true
+            }
+        ] 
+    },
+    {
+        albumSrc: 'img/foto-cover-album/tuneMusicLab-cover-album.jpg',
+        fotoSrc: [
+            {
+                src: 'img/tune-music-lab/tml-1.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/tune-music-lab/tml-2.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/tune-music-lab/tml-3.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/tune-music-lab/tml-4.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/tune-music-lab/tml-5.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/tune-music-lab/tml-6.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/tune-music-lab/tml-7.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/tune-music-lab/tml-8.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/tune-music-lab/tml-9.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/tune-music-lab/tml-10.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/tune-music-lab/tml-11.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/tune-music-lab/tml-12.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/tune-music-lab/tml-13.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/tune-music-lab/tml-14.jpg',
+                isLoading: true
+            }
+        ] 
+    },
+    {
+        albumSrc: 'img/foto-cover-album/rockCamp-cover-album.jpg',
+        fotoSrc: [
+            {
+                src: 'img/rock-camp/rc-1.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/rock-camp/rc-2.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/rock-camp/rc-3.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/rock-camp/rc-4.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/rock-camp/rc-5.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/rock-camp/rc-6.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/rock-camp/rc-7.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/rock-camp/rc-8.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/rock-camp/rc-9.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/rock-camp/rc-10.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/rock-camp/rc-11.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/rock-camp/rc-12.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/rock-camp/rc-13.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/rock-camp/rc-14.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/rock-camp/rc-15.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/rock-camp/rc-16.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/rock-camp/rc-17.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/rock-camp/rc-18.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/rock-camp/rc-19.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/rock-camp/rc-20.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/rock-camp/rc-21.jpg',
+                isLoading: true
+            },
+            {
+                src: 'img/rock-camp/rc-22.jpg',
+                isLoading: true
+            }
+        ] 
+    }
+]
--- js/foto-layout.js
+++ js/foto-layout.js
@@ -1,7 +1,7 @@
 (function() {
     var $window = $(window);
 
-    function centerArrow() {    
+    function centerArrow() {   
         var leftArrow = $('.freccia-album-left');
         var rightArrow = $('.freccia-album-right');
         var value;
@@ -27,19 +27,38 @@
             } else {
                 value = (heightGalleria / 2) - 20;
             }
-            valueText = value.toString() + 'px'
+            valueText = value + 'px'
             leftArrow.css({'top': valueText});
             rightArrow.css({'top': valueText});
         });
+    }
+
+    function centerNavigateArrows() {
+        var navigateLeft = $('.navigate-left');
+        var navigateRight = $('.navigate-right');
+        var heightFotoGalleria = $('.foto-galleria').height();
+        var topValue = (heightFotoGalleria / 2) - 20;
+        navigateLeft.css({'top': topValue});
+        navigateRight.css({'top': topValue});
+
+        $window.on('resize', function() {
+            var heightFotoGalleria = $('.foto-galleria').height();
+            var topValue = (heightFotoGalleria / 2) - 20;
+            navigateLeft.css({'top': topValue});
+            navigateRight.css({'top': topValue});
+        })
     }
 
     function elemWidth() {
         var elemSx = $('.elem-album-sx');
         var elemDx = $('.elem-album-dx');
         var elemCn = $('.elem-album-cn');
+        var elemGalleria = $('.foto-galleria');
         var widthElemSx = elemSx.width();
         var widthElemDx = elemDx.width();
         var widthElemCn = elemCn.width();
+        var widthElemGalleria = elemGalleria.width();
+        elemGalleria.css ({'height': widthElemGalleria / 2});
         if (window.innerWidth < 960) {
             var newWidthElemCn = widthElemCn / 1.4;
             elemCn.css({'height': newWidthElemCn});
@@ -53,6 +72,8 @@
             var widthElemSx = elemSx.width();
             var widthElemDx = elemDx.width();
             var widthElemCn = elemCn.width();
+            var widthElemGalleria = elemGalleria.width();
+            elemGalleria.css ({'height': widthElemGalleria / 2});
             if (window.innerWidth < 960) {
                 var newWidthElemCn = widthElemCn / 1.4;
                 elemCn.css({'height': newWidthElemCn});
@@ -64,10 +85,46 @@
         });
     }
 
-
-
+    function centraturaImg() {
+        var imgGalleria = $('.album-galleria li img');
+        imgGalleria.each(function() {
+            if (this.width > this.height) {
+            $(this).css('width', '100%');
+            } else {
+                $(this).css('height', '100%');
+            }
+        })
+        imgGalleria.each(function() {
+            $(this).css({
+                'margin-left': -this.width / 2,
+                'margin-top': -this.height / 2
+            });
+        })
+        
+        $window.on('resize', function() {
+            imgGalleria.each(function() {
+                if (this.width > this.height) {
+                $(this).css('width', '100%');
+                } else {
+                    $(this).css('height', '100%');
+                }
+            })
+            imgGalleria.each(function() {
+                $(this).css({
+                    'margin-left': -this.width / 2,
+                    'margin-top': -this.height / 2
+                });
+            })
+        })
+    }
 
     elemWidth();
     centerArrow();
+    centerNavigateArrows();
+    centraturaImg();
 
+
+
+
+    
 }())
