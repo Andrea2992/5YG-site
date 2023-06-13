287190060f8b9934dc8765e0b3e4f85e0f01c793
Andrea2992 <redbaron2992@gmail.com>
2023-06-13 16:03:58 +0200

    refactor codice comune

--- js/foto-load.js
+++ js/foto-load.js
@@ -1,16 +1,14 @@
 (function() {
 
-    var selectedAlbumIndex;
+    var selectedAlbumIndex = 0;
     var leftElemAlbum = $('.elem-album-sx img');
     var rightElemAlbum = $('.elem-album-dx img');
     var centerElemAlbum = $('.elem-album-cn img');
     var albumTitle = $('.album-title');
 
-    if (window.innerWidth < 960) {
-        selectedAlbumIndex = 0;
-    } else {
-        selectedAlbumIndex = 1;
-    }
+    function showAlbum() {
+        
+    };
 
     var albumsCover = foto.map(function(f) {
         return f.albumSrc;
@@ -18,140 +16,95 @@
     var albumsTitle = foto.map(function(f) {
         return f.albumTitle;
     });
+    
+    leftElemAlbum.attr({
+        'src': albumsCover[albumsCover.length - 1],
+        'alt':  albumsTitle[albumsCover.length - 1]
+    });
+    centerElemAlbum.attr({
+        'src': albumsCover[selectedAlbumIndex],
+        'alt':  albumsTitle[selectedAlbumIndex]
+    });
+    rightElemAlbum.attr({
+        'src': albumsCover[selectedAlbumIndex + 1],
+        'alt':  albumsTitle[selectedAlbumIndex + 1]
+    });
+    albumTitle.html(albumsTitle[selectedAlbumIndex]);
 
-    if (window.innerWidth < 960) {
+    function changeAlbumCoverImages(leftIndex, centerIndex, rightIndex) {
         leftElemAlbum.attr({
-            'src': albumsCover[albumsCover.length - 1],
-            'alt':  albumsTitle[albumsCover.length - 1]
+            'src': albumsCover[leftIndex],
+            'alt':  albumsTitle[leftIndex]
         });
         centerElemAlbum.attr({
-            'src': albumsCover[selectedAlbumIndex],
-            'alt':  albumsTitle[selectedAlbumIndex]
+            'src': albumsCover[centerIndex],
+            'alt':  albumsTitle[centerIndex]
         });
         rightElemAlbum.attr({
-            'src': albumsCover[selectedAlbumIndex + 1],
-            'alt':  albumsTitle[selectedAlbumIndex + 1]
+            'src': albumsCover[rightIndex],
+            'alt':  albumsTitle[rightIndex]
         });
-        albumTitle.html(albumsTitle[selectedAlbumIndex])
-    } else {
-        leftElemAlbum.attr({
-            'src': albumsCover[selectedAlbumIndex - 1],
-            'alt':  albumsTitle[selectedAlbumIndex - 1]
-        });
-        centerElemAlbum.attr({
-            'src': albumsCover[selectedAlbumIndex],
-            'alt':  albumsTitle[selectedAlbumIndex]
-        });
-        rightElemAlbum.attr({
-            'src': albumsCover[selectedAlbumIndex + 1],
-            'alt':  albumsTitle[selectedAlbumIndex + 1]
-        });
-        albumTitle.html(albumsTitle[selectedAlbumIndex])
     }
 
     $('.freccia-album-left').on('click', function() {
         var currentAlbumIndex;
         if (selectedAlbumIndex == 1) {
             currentAlbumIndex = selectedAlbumIndex - 1;
-            leftElemAlbum.attr({
-                'src': albumsCover[albumsCover.length - 1],
-                'alt':  albumsTitle[albumsCover.length - 1]
-            });
-            centerElemAlbum.attr({
-                'src': albumsCover[currentAlbumIndex],
-                'alt':  albumsTitle[currentAlbumIndex]
-            });
-            rightElemAlbum.attr({
-                'src': albumsCover[currentAlbumIndex + 1],
-                'alt':  albumsTitle[currentAlbumIndex + 1]
-            });
-            albumTitle.html(albumsTitle[currentAlbumIndex])            
+            var leftIndex = albumsCover.length - 1;
+            var centerIndex = currentAlbumIndex;
+            var rightIndex = currentAlbumIndex + 1;
+            changeAlbumCoverImages(leftIndex, centerIndex, rightIndex);
+            albumTitle.html(albumsTitle[currentAlbumIndex]);            
         } else if (selectedAlbumIndex == 0) {
             currentAlbumIndex = albumsCover.length - 1;
-            leftElemAlbum.attr({
-                'src': albumsCover[currentAlbumIndex - 1],
-                'alt':  albumsTitle[currentAlbumIndex - 1]
-            });
-            centerElemAlbum.attr({
-                'src': albumsCover[currentAlbumIndex],
-                'alt':  albumsTitle[currentAlbumIndex]
-            });
-            rightElemAlbum.attr({
-                'src': albumsCover[0],
-                'alt':  albumsTitle[0]
-            });
+            var leftIndex = currentAlbumIndex - 1;
+            var centerIndex = currentAlbumIndex;
+            var rightIndex = 0;
+            changeAlbumCoverImages(leftIndex, centerIndex, rightIndex);
             albumTitle.html(albumsTitle[currentAlbumIndex])
         } else {
             currentAlbumIndex = selectedAlbumIndex - 1;
-            leftElemAlbum.attr({
-                'src': albumsCover[currentAlbumIndex - 1],
-                'alt':  albumsTitle[currentAlbumIndex - 1]
-            });
-            centerElemAlbum.attr({
-                'src': albumsCover[currentAlbumIndex],
-                'alt':  albumsTitle[currentAlbumIndex]
-            });
-            rightElemAlbum.attr({
-                'src': albumsCover[currentAlbumIndex + 1],
-                'alt':  albumsTitle[currentAlbumIndex + 1]
-            });
+            var leftIndex = currentAlbumIndex - 1;
+            var centerIndex = currentAlbumIndex;
+            var rightIndex = currentAlbumIndex + 1;
+            changeAlbumCoverImages(leftIndex, centerIndex, rightIndex);
             albumTitle.html(albumsTitle[currentAlbumIndex])    
         }
         selectedAlbumIndex = currentAlbumIndex;
         fotoAdapt();
+        showAlbum();
     });
 
     $('.freccia-album-right').on('click', function() {
         var currentAlbumIndex;
         if (selectedAlbumIndex < albumsCover.length - 2) {
             currentAlbumIndex = selectedAlbumIndex + 1;
-            leftElemAlbum.attr({
-                'src': albumsCover[currentAlbumIndex - 1],
-                'alt':  albumsTitle[currentAlbumIndex - 1]
-            });
-            centerElemAlbum.attr({
-                'src': albumsCover[currentAlbumIndex],
-                'alt':  albumsTitle[currentAlbumIndex]
-            });
-            rightElemAlbum.attr({
-                'src': albumsCover[currentAlbumIndex + 1],
-                'alt':  albumsTitle[currentAlbumIndex + 1]
-            });
+            var leftIndex = currentAlbumIndex - 1;
+            var centerIndex = currentAlbumIndex;
+            var rightIndex = currentAlbumIndex + 1;
+            changeAlbumCoverImages(leftIndex, centerIndex, rightIndex);
             albumTitle.html(albumsTitle[currentAlbumIndex])            
         } else if (selectedAlbumIndex == albumsCover.length - 2) {
             currentAlbumIndex = selectedAlbumIndex + 1;
-            leftElemAlbum.attr({
-                'src': albumsCover[currentAlbumIndex - 1],
-                'alt':  albumsTitle[currentAlbumIndex - 1]
-            });
-            centerElemAlbum.attr({
-                'src': albumsCover[currentAlbumIndex],
-                'alt':  albumsTitle[currentAlbumIndex]
-            });
-            rightElemAlbum.attr({
-                'src': albumsCover[0],
-                'alt':  albumsTitle[0]
-            });
+            var leftIndex = currentAlbumIndex - 1;
+            var centerIndex = currentAlbumIndex;
+            var rightIndex = 0;
+            changeAlbumCoverImages(leftIndex, centerIndex, rightIndex);
             albumTitle.html(albumsTitle[currentAlbumIndex])
         } else {
             currentAlbumIndex = 0;
-            leftElemAlbum.attr({
-                'src': albumsCover[albumsCover.length - 1],
-                'alt':  albumsTitle[albumsCover.length - 1]
-            });
-            centerElemAlbum.attr({
-                'src': albumsCover[currentAlbumIndex],
-                'alt':  albumsTitle[currentAlbumIndex]
-            });
-            rightElemAlbum.attr({
-                'src': albumsCover[currentAlbumIndex + 1],
-                'alt':  albumsTitle[currentAlbumIndex + 1]
-            });
+            var leftIndex = albumsCover.length - 1;
+            var centerIndex = currentAlbumIndex;
+            var rightIndex = currentAlbumIndex + 1;
+            changeAlbumCoverImages(leftIndex, centerIndex, rightIndex);
             albumTitle.html(albumsTitle[currentAlbumIndex])    
         }
         selectedAlbumIndex = currentAlbumIndex;
         fotoAdapt();
+        showAlbum();
     });    
+
+
 
 
 
