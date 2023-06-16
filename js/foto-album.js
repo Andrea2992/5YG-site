15def9d33dc344fbb3190079823caa9916ad9ffd
Andrea2992 <redbaron2992@gmail.com>
2023-06-16 17:44:08 +0200

    W.I.P. sezione foto

--- js/foto-album.js
+++ js/foto-album.js
@@ -5,43 +5,43 @@ var foto = [
         fotoSrc: [
             {
                 src: 'img/vecchia-formazione/vf-1.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/vecchia-formazione/vf-2.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/vecchia-formazione/vf-3.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/vecchia-formazione/vf-4.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/vecchia-formazione/vf-5.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/vecchia-formazione/vf-6.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/vecchia-formazione/vf-7.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/vecchia-formazione/vf-8.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/vecchia-formazione/vf-9.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/vecchia-formazione/vf-10.jpg',
-                isLoading: true
+                alt: 'true'
             }
         ] 
     },
@@ -51,79 +51,79 @@ var foto = [
         fotoSrc: [
             {
                 src: 'img/torreano/t-1.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/torreano/t-2.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/torreano/t-3.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/torreano/t-4.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/torreano/t-5.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/torreano/t-6.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/torreano/t-7.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/torreano/t-8.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/torreano/t-9.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/torreano/t-10.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/torreano/t-11.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/torreano/t-12.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/torreano/t-13.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/torreano/t-14.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/torreano/t-15.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/torreano/t-16.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/torreano/t-17.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/torreano/t-18.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/torreano/t-19.jpg',
-                isLoading: true
+                alt: 'true'
             }
         ] 
     },
@@ -133,59 +133,59 @@ var foto = [
         fotoSrc: [
             {
                 src: 'img/tune-music-lab/tml-1.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/tune-music-lab/tml-2.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/tune-music-lab/tml-3.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/tune-music-lab/tml-4.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/tune-music-lab/tml-5.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/tune-music-lab/tml-6.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/tune-music-lab/tml-7.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/tune-music-lab/tml-8.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/tune-music-lab/tml-9.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/tune-music-lab/tml-10.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/tune-music-lab/tml-11.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/tune-music-lab/tml-12.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/tune-music-lab/tml-13.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/tune-music-lab/tml-14.jpg',
-                isLoading: true
+                alt: 'true'
             }
         ] 
     },
@@ -195,91 +195,91 @@ var foto = [
         fotoSrc: [
             {
                 src: 'img/rock-camp/rc-1.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/rock-camp/rc-2.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/rock-camp/rc-3.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/rock-camp/rc-4.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/rock-camp/rc-5.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/rock-camp/rc-6.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/rock-camp/rc-7.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/rock-camp/rc-8.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/rock-camp/rc-9.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/rock-camp/rc-10.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/rock-camp/rc-11.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/rock-camp/rc-12.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/rock-camp/rc-13.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/rock-camp/rc-14.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/rock-camp/rc-15.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/rock-camp/rc-16.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/rock-camp/rc-17.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/rock-camp/rc-18.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/rock-camp/rc-19.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/rock-camp/rc-20.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/rock-camp/rc-21.jpg',
-                isLoading: true
+                alt: 'true'
             },
             {
                 src: 'img/rock-camp/rc-22.jpg',
-                isLoading: true
+                alt: 'true'
             }
         ] 
     }
--- js/init.js
+++ js/init.js
@@ -1,6 +1,7 @@
 const slider = document.querySelector(".items");
 const slides = document.querySelectorAll(".item");
 const button = document.querySelectorAll(".button");
+var cacheImg = {};
 
 let current = 0;
 let prev = slides.length - 1;
@@ -12,34 +13,144 @@ var albumsCover = foto.map(function(f) {
 var albumsTitle = foto.map(function(f) {
     return f.albumTitle;
 });
+var albumsPhotos = foto.map(function(f) {
+    return f.fotoSrc;
+})
 
 var albumTitle = $('.album-title');
     albumTitle.html(albumsTitle[0]);
-
 
 function elemWidth() {
     var item = $('.item');
     var widthItem = item.width();
     item.css({'height': widthItem});
 }
-
 $(window).on('resize', function() {
     var item = $('.item');
     var widthItem = item.width();
     item.css({'height': widthItem});
 });
-
 elemWidth();
-
-
 
 for (let i = 0; i < button.length; i++) {
     button[i].addEventListener("click", () => i == 0 ? goToPrev() : goToNext());
 }
 
-function loadAlbumImages(albumIndex) {
-    /*da scrivere*/
+//PARCO GIOCHI
+var imageIds = [];
+
+function createImgElemForAlbum() {
+    var albumsPhotosLength = albumsPhotos.map(function(album) {
+        return album.length;
+    });
+    var maxImagesNum = Math.max(...albumsPhotosLength);
+    var divFotoGalleria = $('#foto-galleria');
+    for (var i = 0; i < maxImagesNum; i++) {
+        var div = $('<div></div>');
+        var imgItem = $('<img>');
+        var id = 'album-image-' + i;
+        imgItem.attr('id', id);
+        div.append(imgItem);
+        divFotoGalleria.append(div);
+        imageIds.push(id)
+    }
+
 }
+
+createImgElemForAlbum();
+/*var cacheImgReale = {
+    'img/foto-cover-album/vecchiaFormazione-cover-album.jpg': [
+        {
+            src: 'img/vecchia-formazione/vf-1.jpg',
+            isLoading: true
+        },
+        {
+            src: 'img/vecchia-formazione/vf-2.jpg',
+            isLoading: true
+        },
+    ]
+}*/
+
+function unloadImages() {
+    //per prima cosa unload variabile images ('' per src e alt e classe hide)
+}
+
+function showLoader() {
+    //mostra loader--nascondi frecce
+}
+
+function addToCache(albumIndex) {
+    var selectedPhotos = albumsPhotos[albumIndex]
+    var albumSrcKey = albumsCover[albumIndex]
+    if (!cacheImg[albumSrcKey]) {
+        cacheImg[albumSrcKey] = [];
+    }
+    for (var i = 0; i < selectedPhotos.length; i++) {
+         var currentPhoto = selectedPhotos[i]
+     
+        var alreadyInCache = cacheImg[albumSrcKey]?.find(function(photo) {
+            return photo.src == currentPhoto.src;
+        })
+        if (!alreadyInCache) {
+            var photoElement = {
+                 src: currentPhoto.src,
+                 alt: currentPhoto.alt,
+                 isLoading: true,
+                 index: i
+             }
+            cacheImg[albumSrcKey].push(photoElement);
+
+        }
+    }
+}
+
+function listenToLoadEvent(albumIndex) {
+    var albumSrcKey = albumsCover[albumIndex];
+    var cachedAlbumPhotos = cacheImg[albumSrcKey];
+    var notLoadedPhotos = cachedAlbumPhotos.filter(function(photo) {
+        return photo.isLoading == true;
+    });
+    var loadedPhotos = cachedAlbumPhotos.filter(function(photo) {
+        return photo.isLoading == false;
+    });
+    for (var i = 0; i < loadedPhotos.length; i++) {
+        var photoIndex = loadedPhotos[i].index;
+        var imageId = imageIds[photoIndex];
+        $('#' + imageId).attr({                          
+            'src': loadedPhotos[i].src,                         
+            'alt': loadedPhotos[i].alt             
+          })//display visible
+    }
+    for (var i = 0; i < notLoadedPhotos.length; i++) {
+        var photoIndex = notLoadedPhotos[i].index;
+        var imageId = imageIds[photoIndex];
+        $('#' + imageId).on('load', function() {
+            var elementInCache = cacheImg[albumSrcKey].find(function(photo) {
+            return photo.src == notLoadedPhotos[i].src;
+            })
+            elementInCache.isLoading = false;
+        })
+         $('#' + imageId).attr({                          
+            'src': notLoadedPhotos[i].src,                         
+            'alt': notLoadedPhotos[i].alt             
+          })//display visible
+    }
+    
+}
+
+function loadAlbumImages(albumIndex) {
+    unloadImages();
+    showLoader();
+    addToCache(albumIndex);
+    listenToLoadEvent(albumIndex);    
+}
+
+   
+
+
+loadAlbumImages(0);
+
+//FINE PARCO GIOCHI
 
 const goToPrev = function() {
     var newCenterIndex  = current > 0 ? current - 1 : slides.length - 1;
