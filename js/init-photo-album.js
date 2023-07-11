bb72915365ab42a4ba83cf98b56868f86f4ab34e
Andrea2992 <redbaron2992@gmail.com>
2023-07-11 15:27:57 +0200

    refactor call-slider-album file

--- js/init-photo-album.js
+++ js/init-photo-album.js
@@ -147,7 +147,7 @@ function listenToLoadEvent(albumIndex) {
             })
             if (allPhotoLoaded) {
                 hideLoader();
-                sliderAlbum();
+                configurePhotoVisibility();
             }
         })
         $('#' + imageId).attr({                          
---
+++ js/visible-photo-manager.js
@@ -0,0 +1,40 @@
+var currentVisiblePhotoIndex = 0;
+
+function getShowedPhoto() {
+    var $allPhotoElements = $('.foto-galleria div img');
+    var allPhotoElements = [];
+    for (i = 0; i < $allPhotoElements.length; i++) {
+        allPhotoElements.push($allPhotoElements[i]);
+    }
+    return allPhotoElements.filter(function(img) {
+        return img.attributes[1].nodeValue !== "";
+    });
+}
+
+function configurePhotoVisibility() {
+    var showedPhoto = getShowedPhoto();
+    $showedPhoto = $(showedPhoto);
+    $showedPhoto.each(function() {
+        $(this).removeClass('hide').addClass('block');
+    })
+    showSelectedPhoto(0)
+}
+
+$('.navigate-left').on('click', function() {
+    var showedPhoto = getShowedPhoto();
+    var newVisiblePhotoIndex = currentVisiblePhotoIndex > 0 ? currentVisiblePhotoIndex -1 : showedPhoto.length - 1;
+    showSelectedPhoto(newVisiblePhotoIndex)
+});
+$('.navigate-right').on('click', function() {
+    var showedPhoto = getShowedPhoto();
+    var newVisiblePhotoIndex = currentVisiblePhotoIndex < showedPhoto.length - 1 ? currentVisiblePhotoIndex + 1 : 0;
+    showSelectedPhoto(newVisiblePhotoIndex)
+});
+
+function showSelectedPhoto(index) {
+    currentVisiblePhotoIndex = index;
+    var imageContainerElements = $('.photoAlbumDiv');
+    imageContainerElements.removeClass('opacityZero');
+    imageContainerElements.addClass('opacityZero');
+    imageContainerElements.eq(index).removeClass('opacityZero');
+}
